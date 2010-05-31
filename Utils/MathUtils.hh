71bc06bdfaf3568af4da29aeb76756a873bab873
Matthias Steinke <matthias@pc12.(none)>
2010-05-31 19:38:10 +0200

    added two more integer pow functions to MathUtils.hh

--- Utils/MathUtils.hh
+++ Utils/MathUtils.hh
@@ -1,6 +1,36 @@
 #ifndef MATHUTILS_HH
 #define MATHUTILS_HH
 
+#include <cassert>
+
+inline double pow(double x, int p)
+{
+  if(p == 0) return 1.0;
+  if(x == 0.0 && p > 0) return 0.0;
+  if(p < 0) {p=-p; x=1/x;}
+	
+  double r = 1.0;
+  for(;;) {
+    if(p & 1) r *= x;
+    if((p >>= 1) == 0)	return r;
+    x *= x;
+  }
+}
+
+inline double pow(double x, unsigned int p)
+{
+  if(p == 0) return 1.0;
+  if(x == 0.0) return 0.0;
+	
+  double r = 1.0;
+  for(;;) {
+    if(p & 1) r *= x;
+    if((p >>= 1) == 0)	return r;
+    x *= x;
+  }
+}
+
+
 inline int pow(int x, int p)
 {
   if(p == 0) return 1;
