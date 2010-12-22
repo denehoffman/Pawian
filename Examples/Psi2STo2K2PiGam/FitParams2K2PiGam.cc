950a3d630c1a37c1f4042cfbe714286511b70a4a
Bertram Kopf <bertram@pc14.(none)>
2010-12-22 16:15:20 +0100

    redesign of the package Examples/Psi2STo2K2PiGam 2

---
+++ Examples/Psi2STo2K2PiGam/FitParams2K2PiGam.cc
@@ -0,0 +1,314 @@
+#include <getopt.h>
+#include <fstream>
+#include <string>
+
+#include "Examples/Psi2STo2K2PiGam/FitParams2K2PiGam.hh"
+#include "Examples/Psi2STo2K2PiGam/Psi2STo2K2PiGamStates.hh"
+#include "qft++/relativistic-quantum-mechanics/Utils.hh"
+#include "ErrLogger/ErrLogger.hh"
+
+FitParams2K2PiGam::FitParams2K2PiGam() 
+{
+  Psi2STo2K2PiGamStates theStates;
+  theStates.print(std::cout);
+
+  _jpclsMap[paramEnum2K2PiGam::ChiGam]=theStates.PsiToChiGamStates();
+  _jpclsMap[paramEnum2K2PiGam::K892K892]=theStates.ChiTo2K892States();
+  _jpclsMap[paramEnum2K2PiGam::KappaKappa]=theStates.ChiTo2K_0_States();
+  _jpclsMap[paramEnum2K2PiGam::K_0_1430K_0_1430]=theStates.ChiTo2K_0_States();
+  _jpclsMap[paramEnum2K2PiGam::K_2_1430K_2_1430]=theStates.ChiTo2K_2_1430States();
+  _jpclsMap[paramEnum2K2PiGam::K_0_1430K_2_1430]=theStates.ChiToK0K2_States();
+  _jpclsMap[paramEnum2K2PiGam::K_1_1400ToK892Pi]=theStates.K1400ToKst1PiStates();
+  _jpclsMap[paramEnum2K2PiGam::K_1_1270ToK892Pi]=theStates.K1400ToKst1PiStates();
+  _jpclsMap[paramEnum2K2PiGam::K_1_1270ToK_0_1430Pi]=theStates.K1ToK0PiStates();
+  _jpclsMap[paramEnum2K2PiGam::K_0_1430K_0_1950]=theStates.ChiTo2K_0_States();
+  _jpclsMap[paramEnum2K2PiGam::KappaK_0_1950]=theStates.ChiTo2K_0_States();
+  _jpclsMap[paramEnum2K2PiGam::f980_pif1710_k]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f980_kf1710_pi]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f980f980]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f980f2200]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f980_pif1370_k]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f980_kf1370_pi]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f1710_pif1370_k]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::f1710_kf1370_pi]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::K_0_2400KToKf980]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::K892K_1_1680]=theStates.ChiTo2K892States();
+  _jpclsMap[paramEnum2K2PiGam::K892K_1_2300]=theStates.ChiTo2K892States();
+  _jpclsMap[paramEnum2K2PiGam::sigmaf980]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::sigmaf1710]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::sigmaf2200]=theStates.ChiTof0f0States();
+  _jpclsMap[paramEnum2K2PiGam::K_0_1460ToK892Pi]=theStates.ChiToK0K0States();
+  _jpclsMap[paramEnum2K2PiGam::K_0_1460ToK_0_1430Pi]=theStates.ChiToK0K0States();
+}
+
+FitParams2K2PiGam::~FitParams2K2PiGam()
+{
+}
+
+std::vector< boost::shared_ptr<const JPCLS> > FitParams2K2PiGam::jpclsVec(unsigned int index){
+  unsigned int nOfAmps=paramEnum2K2PiGam::nAmps;
+  if ( index > paramEnum2K2PiGam::nAmps ) Alert << "index " << index 
+						 << "  number of amplitude parametes paramEnum2K2PiGam::numAmps = " 
+ 						    << nOfAmps << endmsg;
+
+  return _jpclsMap[index]; 
+
+}
+
+std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess >& FitParams2K2PiGam::ampMap(param2K2PiGam& params, unsigned int index){
+  unsigned int nOfAmps=paramEnum2K2PiGam::nAmps;
+  if ( index > paramEnum2K2PiGam::nAmps ) Alert << "index " << index 
+						 << "  number of amplitude parametes paramEnum2K2PiGam::numAmps = " 
+ 						    << nOfAmps << endmsg;
+
+  if (index==paramEnum2K2PiGam::ChiGam) return params.PsiToChiGam;
+  else if (index==paramEnum2K2PiGam::K892K892) return params.ChiTo2K892;
+  else if (index==paramEnum2K2PiGam::KappaKappa) return params.ChiTo2Kappa;
+  else if (index==paramEnum2K2PiGam::K_0_1430K_0_1430) return params.ChiTo2K_0_1430;
+  else if (index==paramEnum2K2PiGam::K_2_1430K_2_1430) return params.ChiTo2K_2_1430;
+  else if (index==paramEnum2K2PiGam::K_0_1430K_2_1430) return params.ChiToK_0_1430_K_2_1430;
+  else if (index==paramEnum2K2PiGam::K_1_1400ToK892Pi) return params.K1400ToK892Pi;
+  else if (index==paramEnum2K2PiGam::K_1_1270ToK892Pi) return params.K_1_1270ToK892Pi;
+  else if (index==paramEnum2K2PiGam::K_1_1270ToK_0_1430Pi) return params.K_1_1270ToK_0_1430Pi;
+  else if (index==paramEnum2K2PiGam::K_0_1430K_0_1950) return params.ChiToK_0_1430K_0_1950; 
+  else if (index==paramEnum2K2PiGam::KappaK_0_1950) return params.ChiToKappaK_0_1950;  
+  else if (index==paramEnum2K2PiGam::f980_pif1710_k) return params.f980_pif1710_k;
+  else if (index==paramEnum2K2PiGam::f980_kf1710_pi) return params.f980_kf1710_pi;
+  else if (index==paramEnum2K2PiGam::f980f980) return params.ChiTof980f980;
+  else if (index==paramEnum2K2PiGam::f980f2200) return params.ChiTof980f2200;
+  else if (index==paramEnum2K2PiGam::f980_pif1370_k) return params.f980_pif1370_k;
+  else if (index==paramEnum2K2PiGam::f980_kf1370_pi) return params.f980_kf1370_pi;
+  else if (index==paramEnum2K2PiGam::f1710_pif1370_k) return params.f1710_pif1370_k;
+  else if (index==paramEnum2K2PiGam::f1710_kf1370_pi) return params.f1710_kf1370_pi;
+  else if (index==paramEnum2K2PiGam::K_0_2400KToKf980) return params.ChiToK_0_2400ToKf980;
+  else if (index==paramEnum2K2PiGam::K892K_1_1680) return params.ChiToK892K1680;
+  else if (index==paramEnum2K2PiGam::K892K_1_2300) return params.ChiToK892K2300;
+  else if (index==paramEnum2K2PiGam::sigmaf980) return params.ChiToSigmaf980;
+  else if (index==paramEnum2K2PiGam::sigmaf1710) return params.ChiToSigmaf1710;
+  else if (index==paramEnum2K2PiGam::sigmaf2200) return params.ChiToSigmaf2200;
+  else if (index==paramEnum2K2PiGam::K_0_1460ToK892Pi) return params.K_0_1460ToK892Pi;
+  else if (index==paramEnum2K2PiGam::K_0_1460ToK_0_1430Pi) return params.K_0_1460ToK_0_1430Pi;
+
+  else{
+    Alert << "index " << index << " not supported !!!" << endmsg;
+    exit(0);
+  }
+
+  return params.PsiToChiGam; //makes the compiler happy
+}
+
+pair<double, double>& FitParams2K2PiGam::massPair(param2K2PiGam& params, unsigned int index){
+  unsigned int nOfAmps=paramEnum2K2PiGam::nAmps;
+  unsigned int nOfMasses=paramEnum2K2PiGam::nMasses;
+  if ( index<paramEnum2K2PiGam::nAmps || index > paramEnum2K2PiGam::nMasses ) Alert << "index " << index 
+										      << "  must be between  " << nOfAmps
+										      << " and " << nOfMasses << endmsg;
+  if (index==paramEnum2K2PiGam::K892) return params.BwK892;
+  else if (index==paramEnum2K2PiGam::Kappa) return params.BwKappa;
+  else if (index==paramEnum2K2PiGam::K_0_1430) return params.BwK_0_1430;
+  else if (index==paramEnum2K2PiGam::K_1_1400) return params.BwK_1_1400;
+  else if (index==paramEnum2K2PiGam::K_2_1430) return params.BwK_2_1430;
+  else if (index==paramEnum2K2PiGam::K_1_1270) return params.BwK_1_1270;
+  else if (index==paramEnum2K2PiGam::f1710) return params.Bwf1710;
+  else if (index==paramEnum2K2PiGam::f2200) return params.Bwf2200;
+  else if (index==paramEnum2K2PiGam::sigma) return params.BwSigma;
+  else if (index==paramEnum2K2PiGam::f1370) return params.Bwf1370;
+  else if (index==paramEnum2K2PiGam::K_0_2400) return params.BwK_0_2400;
+  else if (index==paramEnum2K2PiGam::K_0_1950) return params.BwK_0_1950;
+  else if (index==paramEnum2K2PiGam::K_1_1680) return params.BwK_1_1680;
+  else if (index==paramEnum2K2PiGam::K_1_2300) return params.BwK_1_2300;
+  else if (index==paramEnum2K2PiGam::K_0_1460) return params.BwK_0_1460;
+
+  else{ Alert << "index " << index << " not supported !!!" << endmsg;
+    exit(0);
+  }
+
+  return params.BwK_0_1430; //makes the compiler happy
+}
+
+int FitParams2K2PiGam::setFitParamValDec(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, unsigned int index){
+
+  int resultCount=counter;
+  std::vector< boost::shared_ptr<const JPCLS> >::const_iterator itJPCLS;
+  std::vector< boost::shared_ptr<const JPCLS> > currentStates;
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess >* currentMap=0;
+
+  currentStates = _jpclsMap[index];
+  currentMap = &ampMap(theParamVal, index);
+
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess >::iterator it;
+
+  for ( itJPCLS=currentStates.begin(); itJPCLS!=currentStates.end(); ++itJPCLS){
+    double mag=par[resultCount];
+    resultCount++;
+    
+    double phi=par[resultCount];
+    resultCount++;
+    
+
+    std::pair <double,double> tmpParameter=make_pair(mag,phi);
+    (*currentMap)[(*itJPCLS)]=tmpParameter;
+  }
+
+  return resultCount;
+}
+
+
+int FitParams2K2PiGam::setFitParamValMass(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, unsigned int index){
+
+  int resultCount=counter;
+
+  pair<double, double>* currentPair= & massPair(theParamVal, index);
+
+
+  double currentMass=par[resultCount];
+  resultCount++;
+  double currentWidth=par[resultCount];
+  resultCount++;
+
+  std::pair <double,double> thePair=make_pair(currentMass, currentWidth);
+
+  (*currentPair)=thePair;
+
+  return resultCount;
+}
+
+
+int FitParams2K2PiGam::setFitParamFlatteMass(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, std::string key){
+
+  int resultCount=counter;
+
+  if (key==paramEnum2K2PiGam::name(paramEnum2K2PiGam::f980)){ 
+    double* resultFlatMf980= &theParamVal.Flatf980;
+    double* resultFlatgKK= &theParamVal.Flatf980gKK;
+    double* resultFlatgPiPi= &theParamVal.Flatf980gPiPi;
+    
+    double currentMass=par[resultCount];
+    resultCount++;
+    (*resultFlatMf980)=currentMass;
+    
+    double currentFlatgKK=par[resultCount];
+    resultCount++;
+    (*resultFlatgKK)=currentFlatgKK;
+    
+    double currentFlatgPiPi=par[resultCount];
+    resultCount++;
+    (*resultFlatgPiPi)=currentFlatgPiPi;
+  }
+  else if (key==paramEnum2K2PiGam::name(paramEnum2K2PiGam::phaseSpace)){
+    double* resultPhaseSpace= &theParamVal.phaseSpace;
+    double currentPhaseSpace=par[resultCount];
+    resultCount++;
+    (*resultPhaseSpace)=currentPhaseSpace;
+  }
+  else { Alert << "Key: " << key << " not supported for setting up the Flatte mass parameters!!!" << endmsg;
+    exit(1);
+  }
+
+
+  return resultCount;
+}
+
+void FitParams2K2PiGam::setMnUsrParamsDec(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, unsigned int index){
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > startParams;
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > errParams;
+
+  startParams= ampMap(startVal, index);
+  errParams= ampMap(errVal, index);
+
+
+  int counter=0;
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess >::iterator it;
+  for (it=startParams.begin(); it!=startParams.end(); ++it){
+    boost::shared_ptr<const JPCLS> theJPCLS=it->first;   
+    std::pair <double,double> startPair=it->second;
+    std::pair <double,double> errPair=errParams.find(theJPCLS)->second;
+
+    //now fill the fitParameterMap
+    std::string magStr=theJPCLS->name()+paramEnum2K2PiGam::name(index)+"Mag";
+    std::string phiStr=theJPCLS->name()+paramEnum2K2PiGam::name(index)+"phi";
+
+    double magVal=startPair.first;
+    double phiVal=startPair.second;
+    
+    double magErr=errPair.first;
+    double phiErr=errPair.second;
+    
+    double magMin=magVal-magErr;
+    if (magMin<0.) magMin=0.;
+    
+    upar.Add(magStr, magVal, magErr, magMin, magVal+magErr);
+    upar.Add(phiStr, phiVal, phiErr, -2*M_PI, 2*M_PI);
+
+    counter++;
+  }
+
+}
+
+
+void FitParams2K2PiGam::setMnUsrParamsMass(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, unsigned int index){
+  pair<double, double> startParams;
+  pair<double, double> errParams;
+  startParams=massPair(startVal, index);
+  errParams=massPair(errVal, index);
+
+  //now fill the fitParameterMap
+  std::string massStr=paramEnum2K2PiGam::name(index)+"Mag";
+  std::string widthStr=paramEnum2K2PiGam::name(index)+"Width";
+  
+  double massVal=startParams.first;
+  double widthVal=startParams.second;
+  
+  double massErr=errParams.first;
+  double widthErr=errParams.second;
+
+  double massMin=massVal-3.*massErr;
+  if (massMin<0.) massMin=0.;
+
+  double massMax=massVal+3.*massErr;
+
+  double widthMin=0.; 
+  double widthMax=2*widthVal;
+ 
+  upar.Add(massStr, massVal, massErr, massMin, massMax);
+  upar.Add(widthStr, widthVal, widthErr, widthMin, widthMax);
+}
+
+
+void FitParams2K2PiGam::setMnUsrParamsFlatteMass(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, std::string key){
+ 
+  if (key==paramEnum2K2PiGam::name(paramEnum2K2PiGam::f980))
+    {
+      double start_f980M=startVal.Flatf980;
+      double start_FlatgKK=startVal.Flatf980gKK;
+      double start_FlatgPiPi=startVal.Flatf980gPiPi;
+      double err_f980M=errVal.Flatf980;
+      double err_FlatgKK=errVal.Flatf980gKK;
+      double err_FlatgPiPi=errVal.Flatf980gPiPi;
+      
+      //now fill the fitParameterMap
+      std::string massStr=paramEnum2K2PiGam::name(paramEnum2K2PiGam::f980);
+      std::string gKKStr=paramEnum2K2PiGam::name(paramEnum2K2PiGam::f980gKK);
+      std::string gPiPiStr=paramEnum2K2PiGam::name(paramEnum2K2PiGam::f980gpipi);
+      
+      double massMin=0.97;
+      double massMax=1.2;
+      upar.Add(massStr, start_f980M, err_f980M, massMin, massMax);
+      upar.Add(gKKStr, start_FlatgKK, err_FlatgKK, 0., start_FlatgKK+3*err_FlatgKK);
+      upar.Add(gPiPiStr, start_FlatgPiPi, err_FlatgPiPi, 0., start_FlatgPiPi+3*err_FlatgPiPi);
+    }
+  
+  else if ( key == paramEnum2K2PiGam::name(paramEnum2K2PiGam::phaseSpace)){ 
+    double start_phaseSpace=startVal.phaseSpace;
+    double err_phaseSpace=errVal.phaseSpace;
+    std::string theStr=paramEnum2K2PiGam::name(paramEnum2K2PiGam::phaseSpace);
+    
+    double phaseSpaceMax=start_phaseSpace+3.*err_phaseSpace;
+    if ( phaseSpaceMax > 1. ) phaseSpaceMax=1.;
+    upar.Add(theStr, start_phaseSpace, err_phaseSpace, 0., phaseSpaceMax);
+  }
+  
+  else { Alert << "Key: " << key << " not supported for setting up the Flatte mass parameters!!!" << endmsg;
+    exit(1);
+  } 
+}
---
+++ Examples/Psi2STo2K2PiGam/FitParams2K2PiGam.hh
@@ -0,0 +1,143 @@
+#ifndef _FitParams2K2PiGam_H
+#define _FitParams2K2PiGam_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <complex>
+
+#include <cassert>
+#include <boost/shared_ptr.hpp>
+
+#include "TROOT.h"
+// #include <TSystem.h>
+#include "Examples/Psi2STo2K2PiGam/Psi2STo2K2PiGamData.hh"
+#include "qft++/topincludes/relativistic-quantum-mechanics.hh"
+#include "PwaUtils/DataUtils.hh"
+#include "Utils/PawianCollectionUtils.hh"
+
+#include "Minuit2/MnUserParameters.h"
+
+// using namespace std;
+using namespace ROOT::Minuit2;
+
+struct param2K2PiGam
+{
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > PsiToChiGam;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTo2K892;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTo2Kappa;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTo2K_2_1430;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTo2K_0_1430;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK_0_1430_K_2_1430;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK1400ToK892pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > K1400ToK892Pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > K_1_1270ToK892Pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > K_1_1270ToK_0_1430Pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f980_pif1710_k;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f980_kf1710_pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTof980f980;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiTof980f2200;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToSigmaf980;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToSigmaf1710;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToSigmaf2200;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f980_pif1370_k;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f980_kf1370_pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f1710_pif1370_k;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > f1710_kf1370_pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK_0_2400ToKf980;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK_0_1430K_0_1950;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToKappaK_0_1950;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK892K1680;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > ChiToK892K2300;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > K_0_1460ToK892Pi;
+    std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess > K_0_1460ToK_0_1430Pi;
+
+    pair<double, double> BwK892;
+    pair<double, double> BwKappa;
+    pair<double, double> BwK_2_1430;
+    pair<double, double> BwK_0_1430;
+    pair<double, double> BwK_1_1400;
+    pair<double, double> BwK_1_1270;
+    pair<double, double> Bwf1710;
+    pair<double, double> Bwf2200;
+    pair<double, double> BwSigma;
+    pair<double, double> Bwf1370;
+    pair<double, double> BwK_0_2400;
+    pair<double, double> BwK_2_1950;
+    pair<double, double> BwK_0_1950;
+    pair<double, double> BwK_1_1680;
+    pair<double, double> BwK_1_2300;
+    pair<double, double> BwK_0_1460;
+
+    double Flatf980;
+    double Flatf980gPiPi;
+    double Flatf980gKK;
+
+    double phaseSpace;
+};
+
+struct paramEnum2K2PiGam{
+  enum {ChiGam=0, K892K892, KappaKappa, K_0_1430K_0_1430, K_2_1430K_2_1430, K_0_1430K_2_1430,
+	K_1_1400ToK892Pi, K_1_1270ToK892Pi, K_1_1270ToK_0_1430Pi, K_0_1430K_0_1950, KappaK_0_1950,
+	f980_pif1710_k, f980_kf1710_pi, f980f980, f980f2200, f980_pif1370_k, f980_kf1370_pi, f1710_pif1370_k, f1710_kf1370_pi,
+	K_0_2400KToKf980, K892K_1_1680, K892K_1_2300, 
+	sigmaf980, sigmaf1710, sigmaf2200, K_0_1460ToK892Pi, K_0_1460ToK_0_1430Pi, nAmps,         
+
+	K892=nAmps,Kappa, K_0_1430, K_1_1400, K_2_1430, K_1_1270, 
+	f1710, f2200, sigma, f1370, K_0_2400, K_0_1950, K_1_1680, K_1_2300, K_0_1460, nMasses,
+
+	f980=nMasses,f980gpipi, f980gKK, nFlatte,
+
+	phaseSpace=nFlatte, nPhaseSpace};
+
+  static const std::string& name(unsigned int t)
+  {
+    static std::string fitName[paramEnum2K2PiGam::nPhaseSpace]
+      ={"ChiGam", "K892K892", "KappaKappa", "K_0_1430K_0_1430", "K_2_1430K_2_1430", "K_0_1430K_2_1430",
+	"K_1_1400ToK892Pi", "K_1_1270ToK892Pi", "K_1_1270ToK_0_1430Pi", "K_0_1430K_0_1950", "KappaK_0_1950",
+	"f980_pif1710_k", "f980_kf1710_pi", "f980f980", "f980f2200", "f980_pif1370_k", "f980_kf1370_pi","f1710_pif1370_k", "f1710_kf1370_pi",
+	"K_0_2400KToKf980", "K892K_1_1680", "K892K_1_2300", 
+	"sigmaf980", "sigmaf1710", "sigmaf2200", "K_0_1460ToK892Pi", "K_0_1460ToK_0_1430Pi",
+
+	"K892", "Kappa", "K_0_1430", "K_1_1400", "K_2_1430", "K_1_1270", 
+	"f1710", "f2200", "sigma", "f1370", "K_0_2400", "K_0_1950", "K_1_1680", "K_1_2300", "K_0_1460",
+
+	"f980","f980gpipi", "f980gKK",
+	
+	"phaseSpace"};
+    if (t<0 || t>=paramEnum2K2PiGam::nPhaseSpace) assert(0);
+    return fitName[t];
+  } 
+};
+
+
+
+class FitParams2K2PiGam{
+
+public:
+
+  FitParams2K2PiGam(); 
+
+  virtual ~FitParams2K2PiGam();
+
+  std::vector< boost::shared_ptr<const JPCLS> >  jpclsVec(unsigned int index);
+  std::map< boost::shared_ptr<const JPCLS>, pair<double, double>, pawian::Collection::SharedPtrLess >& ampMap(param2K2PiGam& params, unsigned int index);
+  pair<double, double>& massPair(param2K2PiGam& params, unsigned int index);
+
+  int setFitParamValDec(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, unsigned int index);
+  int setFitParamValMass(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, unsigned int index);
+  int setFitParamFlatteMass(param2K2PiGam& theParamVal, const std::vector<double>& par, int counter, std::string key);
+  void setMnUsrParamsDec(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, unsigned int index);
+  void setMnUsrParamsMass(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, unsigned int index);
+  void setMnUsrParamsFlatteMass(MnUserParameters& upar, param2K2PiGam& startVal,  param2K2PiGam& errVal, std::string key);
+
+protected:
+
+private:
+  std::map < unsigned int, std::vector< boost::shared_ptr<const JPCLS> > > _jpclsMap;
+};
+
+
+#endif
