c0c5026b5e110881e545c3fb715d6da73ef3fd83
Julian Pychy <julian@pc38.ep1.rub.de>
2013-01-16 16:28:31 +0100

    small fix

--- pbarpUtils/pbarpHist.cc
+++ pbarpUtils/pbarpHist.cc
@@ -92,7 +92,6 @@ void pbarpHist::initRootStuff(){
   std::ostringstream rootFileName;
   rootFileName << "./pawianHists" << pbarpEnv::instance()->outputFileNameSuffix() << ".root";
   _theTFile=new TFile(rootFileName.str().c_str(),"recreate");
-  std::cout << "creating file with name" << rootFileName.str() <<std::endl;
 
   std::vector<std::vector<std::string> > histMassNameVec=pbarpEnv::instance()->histMassSystems();
   std::vector<std::vector<std::string> >::iterator itVecStr;
