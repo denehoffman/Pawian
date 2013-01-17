9e6c758709216971cdc99ca8b74af18b0c6c8c9d
Marc Pelizaeus <marc@pc24.ep1.rub.de>
2013-01-17 19:12:36 +0100

    Add Omega->3pi helicity

--- Examples/JpsiToOmegaPhiGam/JpsiToOmegaPhiGamEventList.hh
+++ Examples/JpsiToOmegaPhiGam/JpsiToOmegaPhiGamEventList.hh
@@ -12,14 +12,20 @@
 struct enumJpsiGamX4V{
   enum { Psi=0,  V4_PipPimPi0KpKm_HeliPsi, V4_PipPimPi0_HeliPsi, V4_KpKm_HeliPsi, V4_gamma_HeliPsi,
 	 V4_Pip_HeliPsi, V4_Pim_HeliPsi, V4_Pi0_HeliPsi, V4_Kp_HeliPsi, V4_Km_HeliPsi,
-	 V4_PipPimPi0_HeliPipPimPi0KpKm, V4_KpKm_HeliPipPimPi0KpKm, V4_Pip_HeliPipPimPi0, V4_Kp_HeliKpKm, n4Vecs};
+	 V4_PipPimPi0_HeliPipPimPi0KpKm, V4_KpKm_HeliPipPimPi0KpKm, 
+	 V4_Pip_HeliPipPimPi0, V4_Pim_HeliPipPimPi0, V4_Pi0_HeliPipPimPi0,
+	 V4_Kp_HeliKpKm, 
+	 V4_omegaDecPlaneNormal_HeliPipPimPi0, n4Vecs};
   
   static const std::string& name(unsigned int t)
   {
     static std::string fitName[enumJpsiGamX4V::n4Vecs]
       ={ "Psi",  "PipPimPi0KpKm_HeliPsi", "PipPimPi0_HeliPsi", "KpKm_HeliPsi", "gamma_HeliPsi",
 	 "Pip_HeliPsi", "Pim_HeliPsi", "Pi0_HeliPsi", "Kp_HeliPsi", "Km_HeliPsi",
-	 "PipPimPi0_HeliPipPimPi0KpKm", "KpKm_HeliPipPimPi0KpKm", "Pip_HeliPipPimPi0", "Kp_HeliKpKm"  };
+	 "PipPimPi0_HeliPipPimPi0KpKm", "KpKm_HeliPipPimPi0KpKm", 
+	 "Pip_HeliPipPimPi0",  "Pim_HeliPipPimPi0",  "Pi0_HeliPipPimPi0"
+	 "Kp_HeliKpKm", 
+	 "V4_omegaDecPlaneNormal_HeliPipPimPi0"  };
     
     if (t<0 || t>=enumJpsiGamX4V::n4Vecs) assert(0);
     return fitName[t];
@@ -27,18 +33,29 @@ struct enumJpsiGamX4V{
 };
 
 struct enumJpsiGamXDfunc{
-  enum {Df_XToPhiPhi1=0, Df_XToPhiPhi2, Df_PhiToKsKl,Df_PhiToKpKm, nDfuncts};
+  enum {Df_XToOmegaPhi_KK=0, Df_XToOmegaPhi_3pi, Df_OmegaToPipPimPi0, Df_PhiToKpKm, nDfuncts};
 
   static const std::string& name(unsigned int t)
   {
     static std::string fitName[enumJpsiGamXDfunc::nDfuncts]
-      ={"XToPhiPhi1", "XToPhiPhi2", "PhiToKsKl", "PhiToKpKm"};
+      ={"XToOmegaPhi_KK", "XToOmegaPhi_3pi", "OmegaToPipPimPi0", "PhiToKpKm"};
     if (t<0 || t>=enumJpsiGamXDfunc::nDfuncts) assert(0);
     return fitName[t];
   }
 };
 
+struct enumJpsiGamXKin{
+  enum {OmegaDecLambda=0, nKinVars};
 
+  // static const std::string& name(unsigned int t)
+  // {
+  //   static std::string fitName[enumJpsiGamXKin::nKinVars]
+  //     ={"OmegaDecLambda"};
+  //   if (t<0 || t>=enumJpsiGamXKin::nKinVars) assert(0);
+  //   return fitName[t];
+  // }
+};
+   
 
 class EventList;
 
