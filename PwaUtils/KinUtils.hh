571340f7527dcde2a4b340ab826c3e03ff9ddc7c
Marc Pelizaeus <marc@pc24.ep1.rub.de>
2013-01-25 13:50:37 +0100

    Inverser transformation for helicity vector

--- PwaUtils/KinUtils.hh
+++ PwaUtils/KinUtils.hh
@@ -20,7 +20,35 @@ Vector4<T> helicityVec(const Vector4<T>& cm, const Vector4<T>& mother,
   result.Boost(mother_cm_rot);
 
   return result;
+}  
+
+template<typename T>
+Vector4<T> helicityVecInverse(const Vector4<T>& cm, const Vector4<T>& mother, 
+		       const Vector4<T>& helicityVec) {
+  Vector4<T> mother_cm(mother);
+  mother_cm.Boost(cm);
+  
+  Vector4<T> mother_cm_rot(mother_cm);
+  mother_cm_rot.RotateZ(-mother_cm.Phi());
+  mother_cm_rot.RotateY(-mother_cm.Theta()); 
+
+  Vector4<T> result(helicityVec);
+  result.Boost(-1.* mother_cm_rot);
+  
+  result.RotateY(mother_cm.Theta()); 
+  result.RotateZ(mother_cm.Phi());
+
+  
+  result.Boost(-1.* cm);
+  
+  return result;
 }
+
+
+
+
+
+
 
 template<typename T>
 T costDecHeli(const Vector4<T>& parent,const Vector4<T>& resonance,
@@ -36,3 +64,4 @@ T costDecHeli(const Vector4<T>& parent,const Vector4<T>& resonance,
 
   return cost;
 }
+
