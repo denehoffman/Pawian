3e8a87cdee50a2e764076ec32ba934ad4b84b5b8
Julian Pychy <julian@pc38.ep1.rub.de>
2013-04-04 12:14:54 +0200

    Put Pawian under the GNU General Public License

--- Examples/Tutorial/MinuitFit/MinuitFitFcn.cc
+++ Examples/Tutorial/MinuitFit/MinuitFitFcn.cc
@@ -1,3 +1,26 @@
+//************************************************************************//
+//									  //
+//  Copyright 2013 Bertram Kopf (bertram@ep1.rub.de)			  //
+//  	      	   Julian Pychy (julian@ep1.rub.de)			  //
+//          	   - Ruhr-Universität Bochum 				  //
+//									  //
+//  This file is part of Pawian.					  //
+//									  //
+//  Pawian is free software: you can redistribute it and/or modify	  //
+//  it under the terms of the GNU General Public License as published by  //
+//  the Free Software Foundation, either version 3 of the License, or 	  //
+//  (at your option) any later version.	 	      	  	   	  //
+//									  //
+//  Pawian is distributed in the hope that it will be useful,		  //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of	  //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	  //
+//  GNU General Public License for more details.	      		  //
+//									  //
+//  You should have received a copy of the GNU General Public License     //
+//  along with Pawian.  If not, see <http://www.gnu.org/licenses/>.	  //
+//									  //
+//************************************************************************//
+
 #include "Examples/Tutorial/MinuitFit/MinuitFitFcn.hh"
 #include "Examples/Tutorial/MinuitFit/MinuitFit.hh"
 #include "ErrLogger/ErrLogger.hh"
