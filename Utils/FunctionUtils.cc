e0a0d55dca4afe7365dffc9dea958ee58c7e2bf6
Bertram Kopf <bertram@pc14.ep1.rub.de>
2013-04-04 18:20:07 +0200

    few modifications in FunctionUtils

--- Utils/FunctionUtils.cc
+++ Utils/FunctionUtils.cc
@@ -1,3 +1,5 @@
+#include <algorithm>   
+
 #include "Utils/FunctionUtils.hh"
 #include "Particle/Particle.hh"
 
@@ -8,9 +10,16 @@ namespace FunctionUtils{
   std::string particleListName(std::vector<Particle*>& particleVec){
   std::string result;
 
+  std::vector<std::string> nameVec;
   std::vector<Particle*>::iterator it;
   for(it=particleVec.begin(); it!=particleVec.end(); ++it){
-    result+=(*it)->name();
+    nameVec.push_back((*it)->name());
+  }
+
+  std::sort(nameVec.begin(), nameVec.end());
+  std::vector<std::string>::iterator itStr;
+  for(itStr=nameVec.begin(); itStr!=nameVec.end(); ++itStr){
+    result+=(*itStr);
   }
 
   return result;
