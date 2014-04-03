1e63d499aa0f8050e84b25b50c2d18d46fdb37ff
Bertram Kopf <bertram@pc14.ep1.rub.de>
2014-04-03 15:24:42 +0200

    committed missing new class PwaDynamics/BreitWignerFunction

---
+++ PwaDynamics/BreitWignerFunction.cc
@@ -0,0 +1,35 @@
+//************************************************************************//
+//									  //
+//  Copyright 2014 Bertram Kopf (bertram@ep1.rub.de)			  //
+//  	      	   Julian Pychy (julian@ep1.rub.de)			  //
+//          	   - Ruhr-Universität Bochum 				  //
+//									  //
+//  This file is part of Pawian.					  //
+//									  //
+//  Pawian is free software: you can redistribute it and/or modify	  //
+//  it under the terms of the GNU General Public License as published by  //
+//  the Free Software Foundation, either version 3 of the License, or 	  //
+//  (at your option) any later version.	 	      	  	   	  //
+//									  //
+//  Pawian is distributed in the hope that it will be useful,		  //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of	  //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	  //
+//  GNU General Public License for more details.	      		  //
+//									  //
+//  You should have received a copy of the GNU General Public License     //
+//  along with Pawian.  If not, see <http://www.gnu.org/licenses/>.	  //
+//									  //
+//************************************************************************//
+
+#include "PwaDynamics/BreitWignerFunction.hh"
+
+complex<double>  BreitWignerFunction::NonRel(double currentMass,double mass0, double width){
+  complex<double> i(0.,1.);
+  return mass0*width/(mass0*mass0 - currentMass*currentMass- i*mass0*width);
+}
+
+complex<double> BreitWignerFunction::Rel(double currentMass,double mass0, double width, double massA, double massB){
+  complex<double> i(0.,1.);
+  complex<double> rho0_m=phaseSpaceFac(mass0, massA, massB);
+  return mass0*width/(mass0*mass0 - currentMass*currentMass- i*rho0_m*mass0*width);
+}
---
+++ PwaDynamics/BreitWignerFunction.hh
@@ -0,0 +1,50 @@
+//************************************************************************//
+//									  //
+//  Copyright 2013 Bertram Kopf (bertram@ep1.rub.de)			  //
+//  	      	   Julian Pychy (julian@ep1.rub.de)			  //
+//          	   - Ruhr-Universität Bochum 				  //
+//									  //
+//  This file is part of Pawian.					  //
+//									  //
+//  Pawian is free software: you can redistribute it and/or modify	  //
+//  it under the terms of the GNU General Public License as published by  //
+//  the Free Software Foundation, either version 3 of the License, or 	  //
+//  (at your option) any later version.	 	      	  	   	  //
+//									  //
+//  Pawian is distributed in the hope that it will be useful,		  //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of	  //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	  //
+//  GNU General Public License for more details.	      		  //
+//									  //
+//  You should have received a copy of the GNU General Public License     //
+//  along with Pawian.  If not, see <http://www.gnu.org/licenses/>.	  //
+//									  //
+//************************************************************************//
+
+// Copyright 2014 Bertram Kopf
+
+#pragma once 
+
+//_____________________________________________________________________________
+// @file BreitWignerFunction.hh
+//_____________________________________________________________________________
+
+#include <iostream>
+#include <complex>
+#include <utility>
+#include <memory>
+
+#include "qft++/relativistic-quantum-mechanics/Utils.hh"
+
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+
+namespace BreitWignerFunction { 
+  complex<double> NonRel(double cuurentMass,double mass0, double width);
+  complex<double> Rel(double cuurentMass,double mass0, double width, double massA, double massB);
+  //  complex<double> BlattWRel(double> & p4,double mass, double width);
+
+}; // namespace BreitWignerFunction
+
+
+
