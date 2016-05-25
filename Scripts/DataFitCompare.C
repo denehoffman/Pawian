089b027b3ef66ff695ea207fe1814cb02fe9cb23
Bertram Kopf <bertram@pc14.ep1.rub.de>
2016-05-25 10:21:38 +0200

    changed minor things in DataFitCompare.C

--- Scripts/DataFitCompare.C
+++ Scripts/DataFitCompare.C
@@ -8,6 +8,7 @@
 #include <algorithm>    // std::max
 
 double CalcHistoChi2(TH1F* histoData, TH1F* histoFit);
+double CalcKolmogorov(TH1F* histoData, TH1F* histoFit);
 void DataFitCompare(std::string rootFileName, std::string histname, bool createcanvas=true);
 void DataFitComp2(TH1F* histoData, TH1F* histoFit);
 void DataFitCompareAll(std::string rootFileName, bool saveImage=false);
@@ -44,7 +45,8 @@ void DataFitCompare(std::string rootFileName, std::string histname, bool createc
 
      std::cout << "\n" << std::endl;
    CalcHistoChi2(histoData, histoFit);
-
+   CalcKolmogorov(histoData, histoFit);
+   std::cout << "\n" << std::endl;
    DataFitComp2(histoData, histoFit);
 }
 
@@ -61,19 +63,32 @@ double CalcHistoChi2(TH1F* histoData, TH1F* histoFit){
    int nonzerobins=0;
    for(int i=1;i<=nbins1;i++){
       if(histoData->GetBinContent(i) == 0 || histoFit->GetBinContent(i) == 0)
-	 continue;
+      	 continue;
+      // if(histoData->GetBinContent(i) == 0 && histoFit->GetBinContent(i) == 0)
+      // 	 continue;
 
-      double error = sqrt(histoData->GetBinContent(i) + histoFit->GetBinContent(i));
+
+      //     double error = sqrt(histoData->GetBinContent(i) + histoFit->GetBinContent(i));
+      double error =  sqrt(histoData->GetBinError(i)*histoData->GetBinError(i) + histoFit->GetBinError(i)*histoFit->GetBinError(i));
 
       chi2+=pow(histoData->GetBinContent(i) - histoFit->GetBinContent(i), 2)/(error*error);
       //      std::cout << pow(histoData->GetBinContent(i) - histoFit->GetBinContent(i), 2)/(error*error) << std::endl;
       nonzerobins++;
    }
+   double chi2Root=histoData->Chi2Test(histoFit,"WW");
     std::cout << "Chi2 for histo " << histoData->GetName() << "\t" << chi2/nonzerobins 
-	     << "\tp =" << TMath::Prob(chi2, nonzerobins) << std::endl;
+	     << "\tp =" << TMath::Prob(chi2, nonzerobins) 
+	      << "\tpRoot=" << chi2Root  << std::endl;
 
 
    return chi2;
+}
+
+double CalcKolmogorov(TH1F* histoData, TH1F* histoFit){
+  double ksTestResult = histoData->KolmogorovTest(histoFit);
+  std::cout << "Kolmogorov-Smirnov test for histo " << histoData->GetName() 
+	    << "\tp = " << ksTestResult << std::endl;
+  return ksTestResult;  
 }
 
 void DataFitCompareAll(std::string rootFileName, bool saveImage){
