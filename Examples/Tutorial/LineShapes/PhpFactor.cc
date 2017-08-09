da8ca3f9185ca42d9b8a7f4c8e7c046b020156a9
Markus Kuhlmann <mkuhlmann@ep1.rub.de>
2017-08-09 13:50:48 +0200

    reverting change in Jamfile of Minuit examples

--- Examples/Tutorial/LineShapes/PhpFactor.cc
+++ Examples/Tutorial/LineShapes/PhpFactor.cc
@@ -47,7 +47,7 @@ PhpFactor::PhpFactor(double mass1, double mass2, double massMax) :
 {
   _theTFile=new TFile("PhpFactor.root","recreate");
 
-  PhaseSpaceIsobarBBUnstable* phpBBUnstableRhoPi = new PhaseSpaceIsobarBBUnstable(0.77, 0.135, "BBUnstableRhoPi"); 
+  PhaseSpaceIsobarBBUnstable* phpBBUnstableRhoPi = new PhaseSpaceIsobarBBUnstable(0.77, 0.135, "BBUnstable 0.14 0.14 0.14 0.14 0.749535 0.450249 0.14 0.0 0.0"); 
 
   _CMDefaultComplRealHist2= new TH2F("_CMDefaultComplRealHist2", "CM real default",601, -_massMax, _massMax, 601, -_massMax, _massMax);
   _CMDefaultComplRealHist2->GetYaxis()->SetTitle("Im(#sqrt{s})");
