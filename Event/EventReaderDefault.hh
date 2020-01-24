b37db68163bc939883e3875aff5fc73e9677ef9f
Remco de Boer <remco@ihep.ac.cn>
2020-01-24 10:26:05 +0100

    Style: Apply LLVM conventions through ClangFormat6
    Applied it to EventReaderDefault.cc/hh in
    preparation of implementing read-header functionality

--- Event/EventReaderDefault.hh
+++ Event/EventReaderDefault.hh
@@ -1,55 +1,55 @@
 //************************************************************************//
-//									  //
-//  Copyright 2013 Bertram Kopf (bertram@ep1.rub.de)			  //
-//  	      	   Julian Pychy (julian@ep1.rub.de)			  //
-//          	   - Ruhr-Universität Bochum 				  //
-//									  //
-//  This file is part of Pawian.					  //
-//									  //
-//  Pawian is free software: you can redistribute it and/or modify	  //
+//                                                                        //
+//  Copyright 2013 Bertram Kopf (bertram@ep1.rub.de)                      //
+//               Julian Pychy (julian@ep1.rub.de)                         //
+//               - Ruhr-Universität Bochum                                //
+//                                                                        //
+//  This file is part of Pawian.                                          //
+//                                                                        //
+//  Pawian is free software: you can redistribute it and/or modify        //
 //  it under the terms of the GNU General Public License as published by  //
-//  the Free Software Foundation, either version 3 of the License, or 	  //
-//  (at your option) any later version.	 	      	  	   	  //
-//									  //
-//  Pawian is distributed in the hope that it will be useful,		  //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of	  //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	  //
-//  GNU General Public License for more details.	      		  //
-//									  //
+//  the Free Software Foundation, either version 3 of the License, or     //
+//  (at your option) any later version.                                   //
+//                                                                        //
+//  Pawian is distributed in the hope that it will be useful,             //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//  GNU General Public License for more details.                          //
+//                                                                        //
 //  You should have received a copy of the GNU General Public License     //
-//  along with Pawian.  If not, see <http://www.gnu.org/licenses/>.	  //
-//									  //
+//  along with Pawian.  If not, see <http://www.gnu.org/licenses/>.       //
+//                                                                        //
 //************************************************************************//
 
-// epemEvtReader class definition file. -*- C++ -*-
+// EventReaderDefault class definition file. -*- C++ -*-
 // Copyright 2013 Bertram Kopf
 
 #pragma once
 
 #include "Event/EventReader.hh"
 
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <utility>
-
+#include <vector>
 
 class EventList;
 
-class EventReaderDefault : public EventReader
-{
+class EventReaderDefault : public EventReader {
 public:
   EventReaderDefault();
-  EventReaderDefault(const std::vector<std::string>& files, int particles, int skip, bool useWeight=false);
+  EventReaderDefault(const std::vector<std::string> &files, int particles,
+                     int skip, bool useWeight = false);
 
   virtual ~EventReaderDefault();
 
-  virtual bool fill(EventList& evtList, int evtStart=0, int evtStop=1000000);
-  virtual void setUnit(const std::string& theUnit);
-  virtual void setOrder(const std::string& theOrder);
-  
+  virtual bool fill(EventList &evtList, int evtStart = 0,
+                    int evtStop = 1000000);
+  virtual void setUnit(const std::string &theUnit);
+  virtual void setOrder(const std::string &theOrder);
+
 protected:
   std::vector<std::string> fileNames;
   std::vector<std::string>::const_iterator currentFile;
@@ -61,5 +61,3 @@ protected:
   double _unitScaleFactor;
   bool _energyFirst;
 };
-
-
