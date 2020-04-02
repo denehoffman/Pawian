9aed9dd689ba65723464b10373a87e9eab8f6de6
Malte Albrecht <malte@ep1.rub.de>
2020-04-02 16:37:25 +0200

    updated SetEnv_rub.sh for bash

--- SetEnv_rub.sh
+++ SetEnv_rub.sh
@@ -2,7 +2,7 @@ source /opt/root/6-14.02-SL7-64-gcc9.2.0/bin/thisroot.sh
 
 export TOP_DIR=`pwd | sed -e 's/\/nfs//'`
 export extern=/data/sleipnir/PANDA/PWA
-export LD_LIBRARY_PATH=$TOP_DIR/lib:$extern/libgcc610
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TOP_DIR/lib:$extern/libgcc920-opt
 export BOOST_BUILD_PATH=/data/sleipnir/PANDA/boost-build
 export PATH=$BOOST_BUILD_PATH/bin:$TOP_DIR/bin:$PATH
 
