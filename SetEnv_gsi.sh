beccc319e14899d4abfc2bc0037dc3fdf2326150
Malte Albrecht <malte@ep1.rub.de>
2020-05-12 10:00:24 +0200

    remove julia dependency

--- SetEnv_gsi.sh
+++ SetEnv_gsi.sh
@@ -5,9 +5,6 @@ export extern=/lustre/nyx/panda/malte/extern
 export LD_LIBRARY_PATH=$TOP_DIR/lib:$extern/lib:$ROOTSYS/lib
 export BOOST_BUILD_PATH=/lustre/nyx/panda/malte/extern/boost-build/
 export PATH=$ROOTSYS/bin:$TOP_DIR/bin:$PATH
-export JULIA_DIR=/lustre/nyx/panda/malte/extern/juliaInstall/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JULIA_DIR/lib/julia:$LD_LIBRARY_PATH:$JULIA_DIR/lib
-export JULIA_HOME=$JULIA_DIR/bin
 
 rm Jamroot
 ln -s JamrootSL6_gsi Jamroot
