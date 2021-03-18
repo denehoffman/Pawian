cacaae4219308ec147f153b40dd9f846c17c2107
Bertram Kopf <bertram@ep1.rub.de>
2021-03-18 13:14:24 +0100

    minor correction in TMatrixGeneral

--- KMatrixExtract/TMatrixGeneral.cc
+++ KMatrixExtract/TMatrixGeneral.cc
@@ -255,9 +255,8 @@ void TMatrixGeneral::process(){
 	
 	_ArgandH2Vec.at(i)->Fill( currentRho.real()*(*_tMatr)(i,i).real(), 
 				  currentRho.real()*(*_tMatr)(i,i).imag());
-	//double currentphase=180.*atan2((*_tMatr)(i,i).imag(),(*_tMatr)(i,i).real()) / 3.1415;
-        double currentphase = KMatrixFunctions::deltaArgand(_tMatr, mass, i, _orbitalL);
-	complex<double> SijRel_phi=complex<double>(1.,0.)+2.*PawianConstants::i*currentRho.real() *
+	double currentphase=180.*atan2((*_tMatr)(i,i).imag(),(*_tMatr)(i,i).real()) / 3.1415;
+       	complex<double> SijRel_phi=complex<double>(1.,0.)+2.*PawianConstants::i*currentRho.real() *
 	  (*_tMatr)(i,i);
 	if(i==0) {
 	  oStream_phi << mass << "\t" 
