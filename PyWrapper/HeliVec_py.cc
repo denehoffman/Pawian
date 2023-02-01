76c24c3d7ae7aa33f74cac7280ed3934e694bd44
Bertram Kopf <bertram@ep1.rub.de>
2023-02-01 13:43:08 +0100

    modified HeliVec_py wrapper

--- PyWrapper/HeliVec_py.cc
+++ PyWrapper/HeliVec_py.cc
@@ -9,13 +9,50 @@ HeliVec_py::HeliVec_py() :
 {
 }
 
-void HeliVec_py::calcHeliVec() {
+boost::python::tuple HeliVec_py::calcHeliVec() {
   std::cout << "_motherRefQftVec4: " << _motherRefQftVec4 << std::endl;
   std::cout << "_refQftVec4: " << _refQftVec4 << std::endl; 
   std::cout << "_motherQftVec4: " << _motherQftVec4 << std::endl;
   std::cout << "_daughterQftVec4: " << _daughterQftVec4 << std::endl;
   Vector4<double> daughterHeliVec4Qft = KinUtils::heliVec(_motherRefQftVec4, _refQftVec4, _motherQftVec4, _daughterQftVec4);
   std::cout << "daughterHeliVec4Qft: " << daughterHeliVec4Qft << std::endl;
+  return boost::python::make_tuple(daughterHeliVec4Qft.Px(), daughterHeliVec4Qft.Py(), daughterHeliVec4Qft.Pz(), daughterHeliVec4Qft.E());
 }
+
+
+void HeliVec_py::setMother(double px, double py, double pz , double e){
+  _motherQftVec4=Vector4<double>(e, px, py, pz);
+}
+
+void HeliVec_py::setDaughter(double px, double py, double pz , double e){
+  _daughterQftVec4=Vector4<double>(e, px, py, pz);
+}
+
+
+void HeliVec_py::setMotherRef(double px, double py, double pz , double e){
+  _motherRefQftVec4=Vector4<double>(e, px, py, pz);
+}
+
+void HeliVec_py::setRef(double px, double py, double pz , double e){
+  _refQftVec4=Vector4<double>(e, px, py, pz);
+}
+
+boost::python::tuple HeliVec_py::getMother(){
+  return boost::python::make_tuple(_motherQftVec4.Px(), _motherQftVec4.Py(), _motherQftVec4.Pz(), _motherQftVec4.E());
+}
+
+boost::python::tuple HeliVec_py::getDaughter(){
+  return boost::python::make_tuple(_daughterQftVec4.Px(), _daughterQftVec4.Py(), _daughterQftVec4.Pz(), _daughterQftVec4.E());
+}
+
+
+boost::python::tuple HeliVec_py::getMotherRef(){
+  return boost::python::make_tuple(_motherRefQftVec4.Px(), _motherRefQftVec4.Py(), _motherRefQftVec4.Pz(), _motherRefQftVec4.E());
+}
+
+boost::python::tuple HeliVec_py::getRef(){
+  return boost::python::make_tuple(_refQftVec4.Px(), _refQftVec4.Py(), _refQftVec4.Pz(), _refQftVec4.E());
+}
+
 
 
--- PyWrapper/HeliVec_py.hh
+++ PyWrapper/HeliVec_py.hh
@@ -13,7 +13,15 @@ public:
   
   HeliVec_py();
   ~HeliVec_py(){};
-  void calcHeliVec();
+  boost::python::tuple calcHeliVec();
+  void setMother(double px, double py, double pz , double e);
+  void setDaughter(double px, double py, double pz , double e);
+  void setMotherRef(double px, double py, double pz , double e);
+  void setRef(double px, double py, double pz , double e);
+  boost::python::tuple getMother();
+  boost::python::tuple getDaughter();
+  boost::python::tuple getMotherRef();
+  boost::python::tuple getRef();
 
 private:
   Vector4<double> _motherRefQftVec4;
@@ -25,7 +33,15 @@ private:
 BOOST_PYTHON_MODULE(HeliVec_py)
 {
     class_<HeliVec_py>("HeliVec_py")
-        .def("calcHeliVec", &HeliVec_py::calcHeliVec)
-     ;
+      .def("calcHeliVec", &HeliVec_py::calcHeliVec)
+      .def("setMother", &HeliVec_py::setMother)
+      .def("setDaughter", &HeliVec_py::setDaughter)
+      .def("setMotherRef", &HeliVec_py::setMotherRef)
+      .def("setRef", &HeliVec_py::setRef)
+      .def("getMother", &HeliVec_py::getMother)
+      .def("getDaughter", &HeliVec_py::getDaughter)
+      .def("getMotherRef", &HeliVec_py::getMotherRef)
+      .def("getRef", &HeliVec_py::getRef)
+      ;
 }
 
