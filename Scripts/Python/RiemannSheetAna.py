61aef4c0930b9b1a650f0bb8982391256d6eea69
Bertram Kopf <bertram@ep1.rub.de>
2023-02-09 21:22:00 +0100

    modified script RiemannSheetAna.py

--- Scripts/Python/RiemannSheetAna.py
+++ Scripts/Python/RiemannSheetAna.py
@@ -13,7 +13,7 @@ theRiemannSheetAna = rs.RiemannSheetAna_py()
 
 # result = theRiemannSheetAna.calcTMat(1.3, -0.2)
 
-theRiemannSheetAna.SetParamValue("a21700Mass", 1.4)
+# theRiemannSheetAna.SetParamValue("a21700Mass", 1.4)
 
 colors=['#840000', '#ffab0f', '#b6c406', '#89a203', '#01889f', '#014182', '#9e0168', 'red']
 colorm = LinearSegmentedColormap.from_list('my_map', colors, N=200)
@@ -43,7 +43,9 @@ xi = np.linspace(re_Min, re_Max, re_nBins)
 yi = np.linspace(im_Max, im_Min, im_nBins)
 zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='cubic')
 
-fig, ax = plt.subplots(figsize=(12,3))
+# fig, ax = plt.subplots(figsize=(10,3))
+fig, (ax, ax1) = plt.subplots(2,1)
+
 #ax.set_title("Sheet ----")
 ax.set_xlim(re_Min, re_Max)
 ax.set_ylim(im_Max, im_Min)
@@ -56,9 +58,42 @@ Norm = cm.colors.Normalize(vmax=max(z), vmin=0.)
 surf_plotf = ax.contourf(xi, yi, zi, 10, norm = Norm, cmap=colorm, levels = np.arange(0., max(z), 0.02))
 surf_plot = ax.contour(xi, yi, zi, 10, cmap=colorm, linewidths=(0.5), levels = np.arange(0., max(z), 0.02), alpha=0.5)
 
-cbar = fig.colorbar(surf_plotf, fraction=0.046, pad=0.04)
-cbar.ax.set_ylabel("$|T|^2$")
+# cbar = fig.colorbar(surf_plotf, fraction=0.046, pad=0.04)
+# cbar.ax.set_ylabel("$|T|^2$")
+
+theRiemannSheetAna.SetParamValue("a21700Mass", 1.4)
+
+x1 = []
+y1 = []
+z1 = []
+
+for eReal in np.arange(re_Min, re_Max, re_binning) :
+    for eImag in np.arange(im_Min,im_Max, -im_binning) :
+        result = theRiemannSheetAna.calcTMat(eReal, eImag)
+        x1.append(eReal)
+        y1.append(eImag)
+        z1.append(result)
+
+x1i = np.linspace(re_Min, re_Max, re_nBins)
+y1i = np.linspace(im_Max, im_Min, im_nBins)
+z1i = griddata((x1, y1), z1, (x1i[None, :], y1i[:, None]), method='cubic')        
+
+ax1.set_xlim(re_Min, re_Max)
+ax1.set_ylim(im_Max, im_Min)
+
+ax1.set_xlabel("Re(s)")
+ax1.set_ylabel("Im(s)")
+
+Norm1 = cm.colors.Normalize(vmax=max(z1), vmin=0.)
+surf_plotf1 = ax1.contourf(x1i, y1i, z1i, 10, norm = Norm1, cmap=colorm, levels = np.arange(0., max(z), 0.02))
+surf_plot1 = ax1.contour(x1i, y1i, z1i, 10, cmap=colorm, linewidths=(0.5), levels = np.arange(0., max(z), 0.02), alpha=0.5)
+
+# cbar1 = fig.colorbar(surf_plotf1, fraction=0.046, pad=0.04)
+# cbar1.ax1.set_ylabel("$|T|^2$")
 plt.show()
+
+
+# plt.clf()
 
 # theRiemannSheetAna.calcResidue()
 
